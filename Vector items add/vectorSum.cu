072bd4804d54f2ae917ff3ec07b28bdc883561f5
Anderson <aaochoa@utp.edu.co>
2015-04-09 16:14:29 -0500

    Added de data analized in the SumReduction

--- Vector items add/vectorSum.cu
+++ Vector items add/vectorSum.cu
@@ -3,14 +3,14 @@
 #include<time.h>
 #include<cuda.h>
 #include<iostream>
-
+#include<math.h>
 #define BLOCK_SIZE 1024 // Because it's just an array, 1 dimension
 
 using namespace std;
 //====== Serial vector ADD =====================================================
-float serialVectorItemsAdd (float *A, int length)
+double serialVectorItemsAdd (double *A, int length)
 {
-  float sum=0;
+  double sum=0;
 
   for (int i = 0; i < length; i++)
   {
@@ -20,7 +20,7 @@ float serialVectorItemsAdd (float *A, int length)
 }
 
 //====== Function made to print vector =========================================
-void printVector (float *A, int length)
+void printVector (double *A, int length)
 {
   for (int i=0; i<length; i++)
   {
@@ -30,7 +30,7 @@ void printVector (float *A, int length)
 }
 
 //====== Function made to fill the vector with some given value ================
-void fillVector (float *A, float value, int length)
+void fillVector (double *A, double value, int length)
 {
   for (int i=0; i<length; i++)
   {
@@ -39,7 +39,7 @@ void fillVector (float *A, float value, int length)
 }
 
 //====== To compare both results parallel and serial ===========================
-void resultCompare(float A, float  *B)
+void resultCompare(double A, double  *B)
 {
   if(fabs(A-B[0]) < 0.1)
   {
@@ -52,14 +52,15 @@ void resultCompare(float A, float  *B)
 
 //======= Reduction kernel =====================================================
 //Parallel
-__global__ void reduceKernel(float *g_idata, float *g_odata, int length)
+__global__ void reduceKernel(double *g_idata, double *g_odata, int length)
 {
-  __shared__ float sdata[BLOCK_SIZE];
+  __shared__ double sdata[BLOCK_SIZE];
   // each thread loads one element from global to shared mem
   unsigned int tid = threadIdx.x;
-  unsigned int i = blockIdx.x*(blockDim.x*2) + threadIdx.x;
-  if(i<length){
-    sdata[tid] = g_idata[i] + g_idata[i+blockDim.x];
+  unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
+  if(i<length)
+  {
+    sdata[tid] = g_idata[i];
   } else
   {
     sdata[tid] = 0.0;
@@ -81,31 +82,31 @@ __global__ void reduceKernel(float *g_idata, float *g_odata, int length)
   }
 }
 //====== Function made to call the reduction kernel ============================
-void vectorItemsAdd(float *A, float *B, int length)
+void vectorItemsAdd(double *A, double *B, int length)
 {
-  float * d_A;
-  float * d_B;
-  float * algo = (float *) malloc(length * sizeof(float));
+  double * d_A;
+  double * d_B;
+  double * algo = (double *) malloc(length * sizeof(double));
 
-  cudaMalloc(&d_A,length*sizeof(float));
-  cudaMalloc(&d_B,length*sizeof(float));
+  cudaMalloc(&d_A,length*sizeof(double));
+  cudaMalloc(&d_B,length*sizeof(double));
 
-  cudaMemcpy(d_A, A,length*sizeof(float),cudaMemcpyHostToDevice);
-  cudaMemcpy(d_B, B,length*sizeof(float),cudaMemcpyHostToDevice);
+  cudaMemcpy(d_A, A,length*sizeof(double),cudaMemcpyHostToDevice);
+  cudaMemcpy(d_B, B,length*sizeof(double),cudaMemcpyHostToDevice);
 
   int aux=length;
   while(aux>1)
   {
      dim3 dimBlock(BLOCK_SIZE,1,1);
-     int grid=ceil(aux/(float)BLOCK_SIZE);
+     int grid=ceil(aux/(double)BLOCK_SIZE);
       dim3 dimGrid(grid,1,1);
      reduceKernel<<<dimGrid,dimBlock>>>(d_A,d_B,aux);
      cudaDeviceSynchronize();
-     cudaMemcpy(d_A,d_B,length*sizeof(float),cudaMemcpyDeviceToDevice);
-     aux=(ceil(aux/(float)BLOCK_SIZE));
+     cudaMemcpy(d_A,d_B,length*sizeof(double),cudaMemcpyDeviceToDevice);
+     aux=ceil(aux/(double)BLOCK_SIZE);
   }
 
-  cudaMemcpy(B,d_B,length*sizeof(float),cudaMemcpyDeviceToHost);
+  cudaMemcpy(B,d_B,length*sizeof(double),cudaMemcpyDeviceToHost);
 
   cudaFree(d_A);
   cudaFree(d_B);
@@ -114,35 +115,39 @@ void vectorItemsAdd(float *A, float *B, int length)
 //======= MAIN function ========================================================
 int main ()
 {
- int l = 1096000; //Vector's length
- clock_t start, finish;
- double elapsedSecuential, elapsedParallel, optimization;
- float *A = (float *) malloc(l * sizeof(float));
- float *B = (float *) malloc(l * sizeof(float));
+ for(int i=0; i<=29;i++)//to execute the program many times
+ {	cout<<"=> EXECUTION #"<<i<<endl;
+  	unsigned int l = pow(2,i); //Vector's length
+  	cout<<"Matrix size= "<<l<<endl;
+ 		clock_t start, finish;
+ 		double elapsedSecuential, elapsedParallel, optimization;
+   	double *A = (double *) malloc(l * sizeof(double));
+   	double *B = (double *) malloc(l * sizeof(double));
 
- fillVector(A,1.0,l);
- fillVector(B,0.0,l);
+   fillVector(A,1.0,l);
+   fillVector(B,0.0,l);
 
- start = clock();
- float sum = serialVectorItemsAdd(A,l);
- finish = clock();
- cout<< "The result is: " << sum << endl;
- elapsedSecuential = (((double) (finish - start)) / CLOCKS_PER_SEC );
- cout<< "The Secuential process took: " << elapsedSecuential << " seconds to execute "<< endl<< endl;
+   start = clock();
+   double sum = serialVectorItemsAdd(A,l);
+   finish = clock();
+   cout<< "The result is: " << sum << endl;
+   elapsedSecuential = (((double) (finish - start)) / CLOCKS_PER_SEC );
+   cout<< "The Secuential process took: " << elapsedSecuential << " seconds to execute "<< endl<< endl;
 
- start = clock();
- vectorItemsAdd(A,B,l);
- finish = clock();
- cout<< "The result is: " << B[0] << endl;
- elapsedParallel = (((double) (finish - start)) / CLOCKS_PER_SEC );
- cout<< "The Parallel process took: " << elapsedParallel << " seconds to execute "<< endl<< endl;
+   start = clock();
+   vectorItemsAdd(A,B,l);
+   finish = clock();
+   cout<< "The result is: " << B[0] << endl;
+   elapsedParallel = (((double) (finish - start)) / CLOCKS_PER_SEC );
+   cout<< "The Parallel process took: " << elapsedParallel << " seconds to execute "<< endl<< endl;
 
- optimization = elapsedSecuential/elapsedParallel;
- cout<< "The acceleration we've got: " << optimization <<endl;
+   optimization = elapsedSecuential/elapsedParallel;
+   cout<< "The acceleration we've got: " << optimization <<endl;
 
- cout<< "============================================ "<<endl;
- resultCompare(sum, B);
+   resultCompare(sum, B);
+	 cout<< "============================================ "<<endl;
 
- free(A);
- free(B);
+   free(A);
+   free(B);
+ }
 }
